233c32383bd9d585e17ef9b5ad47130307b916f8
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-09-16 21:37:50 -0700

    first commit setup Laravel project, setup first API connection, setup Vue functionality

--- rock/resources/views/welcome.blade.php
+++ rock/resources/views/welcome.blade.php
@@ -26,11 +26,11 @@
                 var settings = {
                     "async":true,
                     "crossDomain": true, 
-                    'url':'https://api.themoviedb.org/3/movie/popular?api_key=d1da20fbfa65312b857fb7b517bf855c&language=en-US&page=1'+pageNumber,
+                    'url':'https://api.themoviedb.org/3/movie/popular?api_key=c&language=en-US&page=1'+pageNumber,
                     "method": "GET",
                     "headers": {
                         "content-type":"application/json;charset=utf-8",
-                        "authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWRhMjBmYmZhNjUzMTJiODU3ZmI3YjUxN2JmODU1YyIsInN1YiI6IjVmNjJlMGI2MWJmMjY2MDAzYWU0Njg3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j-vJcybvOcmdwWH5_ZX1VQ7j38XFJJP2jZH9pXSzxmA"
+                        "authorization":"Bearer "
                     }, 
                     "processData":false,
                     "data": "{}"
